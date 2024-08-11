509d0e1da25169b1b7168049f473da2dbf074513
sarathchandrikak <sarathchandrikaksc@gmail.com>
2024-08-11 21:02:39 +0530

    Add project files.

---
+++ Bloggie.Web/Controllers/AdminTagsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Bloggie.Web.Models.ViewModels;
+using Bloggie.Web.Data;
+using Bloggie.Web.Models.Domain;
+
+namespace Bloggie.Web.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        private BloggieDbContext bloggieDbContext;
+        public AdminTagsController(BloggieDbContext bloggieDbContext) 
+        { 
+           this.bloggieDbContext = bloggieDbContext;
+        }    
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ActionName("Add")]
+        public IActionResult Add(AddTagRequest addTagRequest)
+        {
+            // Mapping addTagRequest to Tag domain model
+            var tag = new Tag
+            {
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName
+            };
+
+            bloggieDbContext.Tags.Add(tag);
+            bloggieDbContext.SaveChanges();
+
+            return View("Add");
+        }
+    }
+}
